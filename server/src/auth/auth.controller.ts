35f8f324096b71975c37b6b19560f4c93daf102b
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-20 13:27:59 +0800

    Update auth.controller.ts

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './../user/user.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/dto/user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private userService: UserService) {}
+
+  @Post('register')
+  async registerUser(@Body() dto: CreateUserDto) {
+    return await this.userService.create(dto);
+  }
+}
