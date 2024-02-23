7e428df3aa56824c706a6a875ed91fccd2845cc1
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-23 09:16:19 +0800

    Number to String Validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(5000);
   console.log(`Server is running on http://localhost:5000`);
-
-  app.enableCors();
 }
 bootstrap();
